3c7fd8354cdb46ccd8ec7cc6d043a39c8937ec60
Ringo37 <yajima.haruhiko@gmail.com>
2025-03-22 17:35:56 +0900

    アクションバーの変更

--- app/components/actionbar.tsx
+++ app/components/actionbar.tsx
@@ -1,37 +1,62 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Form } from "@remix-run/react";
+import Avatar from "boring-avatars";
 import { Button } from "./ui/button";
 import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
 
-interface ActionBarProps {
-  onReturnToCurrentLocation: () => void;
-}
-
-export default function ActionBar({ onReturnToCurrentLocation }: ActionBarProps) {
+export default function ActionBar({
+  username,
+  avatorUrl,
+  uuid,
+}: {
+  username: string;
+  avatorUrl: string | null;
+  uuid: string;
+}) {
+  const [isClient, setIsClient] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
 
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) return null;
+
   return (
-    <div className="fixed top-4 right-5 z-10">
+    <div className="fixed top-4 right-5">
       <Popover open={isOpen} onOpenChange={setIsOpen}>
         <PopoverTrigger asChild>
-          <Button
-            variant="ghost"
-            className="rounded-full bg-gray-800 text-white shadow-lg hover:bg-gray-700 transition"
-            style={{ height: "4rem", width: "4rem" }}
-          >
-            {isOpen ? "✖" : "☰"}
-          </Button>
+          <div>
+            {isOpen ? (
+              <Button
+                variant="ghost"
+                className="rounded-full bg-gray-800 text-white shadow-lg hover:bg-gray-700 transition"
+                style={{ height: "4rem", width: "4rem" }}
+              >
+                ✖
+              </Button>
+            ) : avatorUrl ? (
+              <img src={avatorUrl} alt={username} />
+            ) : (
+              <Avatar size={"4rem"} name={uuid} variant="beam" />
+            )}
+          </div>
         </PopoverTrigger>
-        <PopoverContent className="w-40 p-4 space-y-2">
+        <PopoverContent
+          className="w-60 h-auto p-4"
+          style={{ alignItems: "center", display: "flex", flexFlow: "column" }}
+        >
+          <div className="p-1">
+            {avatorUrl ? (
+              <img src={avatorUrl} alt={username} />
+            ) : (
+              <Avatar size={"5rem"} name={uuid} variant="beam" />
+            )}
+          </div>
+          <div>アイコンを変更する</div>
           <Form action="/logout" method="post">
-            <Button className="w-full">ログアウト</Button>
+            <Button>ログアウト</Button>
           </Form>
-          <Button
-            className="w-full"
-            onClick={onReturnToCurrentLocation} // 現在地に戻るボタン
-          >
-            現在地に戻る
-          </Button>
         </PopoverContent>
       </Popover>
     </div>
