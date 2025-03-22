8b698200d488e788bbf2098953a5962346987f7d
oracle03-jp <youmu.takeru@gmail.com>
2025-03-23 02:23:59 +0900

    画面下部のボタンの修正

--- app/routes/mymap.tsx
+++ app/routes/mymap.tsx
@@ -242,12 +242,14 @@ export default function MapPage() {
     return () => map.remove();
   }, [mapboxToken, memos]);
 
-  const handleZoomIn = () => {
-    mapRef.current?.zoomIn();
+  const handleMakeFriend = () => {
+    /*フレンド追加機能 */
+  }
+
+  const handleSearchMemo = () => {
+    /*メモの検索機能 */
   };
-  const handleZoomOut = () => {
-    mapRef.current?.zoomOut();
-  };
+
   const handleGoToCurrentLocation = () => {
     if (currentLocation && mapRef.current) {
       mapRef.current?.flyTo({
@@ -325,8 +327,8 @@ export default function MapPage() {
       </div>
       <ActionBar username={username!} uuid={uuid!} initialAvatarUrl={avatarUrl} />
       <Bar
-        handleZoomIn={handleZoomIn}
-        handleZoomOut={handleZoomOut}
+        handleMakeFriend={handleMakeFriend}
+        handleSearchMemo={handleSearchMemo}
         handleGoToCurrentLocation={handleGoToCurrentLocation}
       />
       {showModal && (
