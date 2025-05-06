3c622a0d5a7f18f572f95ca7de7983ab13f4c0bc
Ringo37 <yajima.haruhiko@gmail.com>
2025-05-07 00:38:35 +0900

    画像のサイズを調整

--- app/components/userpanel/ProfileSection.tsx
+++ app/components/userpanel/ProfileSection.tsx
@@ -2,7 +2,6 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPen } from "@fortawesome/free-solid-svg-icons";
 import Avatar from "boring-avatars";
 import { Input } from "~/components/ui/input";
-import { Button } from "~/components/ui/button";
 
 type Props = {
   uuid: string;
@@ -32,76 +31,82 @@ export default function ProfileSection({
   setUname,
   handleFileChange,
   handleCopy,
-  handleUpload,
-  setIsProfileChange,
 }: Props) {
+  const imgUrl = avatarUrl
+    ? (() => {
+        const url = new URL(avatarUrl, window.location.origin);
+        url.searchParams.set("h", "500");
+        return url.toString();
+      })()
+    : null;
   return (
     <>
-    <div className="flex flex-col items-center justify-center mb-6">
-      <div className="p-4 relative inline-block">
-        {isProfileChange && (
-          <input
-            type="file"
-            id="fileInput"
-            accept="image/*"
-            ref={fileInputRef}
-            className="opacity-0 absolute w-0 h-0"
-            onChange={handleFileChange}
-          />
-        )}
-        <label htmlFor="fileInput" className="cursor-pointer">
-          {previewUrl ? (
-            <img
-              src={previewUrl}
-              alt={username}
-              className="rounded-full"
-              style={{ height: "7rem", width: "7rem" }}
+      <div className="flex flex-col items-center justify-center mb-6">
+        <div className="p-4 relative inline-block">
+          {isProfileChange && (
+            <input
+              type="file"
+              id="fileInput"
+              accept="image/*"
+              ref={fileInputRef}
+              className="opacity-0 absolute w-0 h-0"
+              onChange={handleFileChange}
             />
-          ) : avatarUrl ? (
-            <img
-              src={avatarUrl}
-              alt={username}
-              className="rounded-full"
-              style={{ height: "7rem", width: "7rem" }}
-            />
-          ) : (
-            <Avatar size="7rem" name={uuid} variant="beam" />
           )}
-        </label>
-        {isProfileChange && (
-          <FontAwesomeIcon
-            icon={faPen}
-            className="absolute top-0 right-0 bg-white p-1 rounded-full shadow  -translate-x-2/3 translate-y-2/3"
-          />
-        )}
-      </div>
-
-      {isProfileChange ? (
-        <div className="flex flex-col">
-          <Input
-            id="username"
-            name="username"
-            type="text"
-            value={uname}
-            onChange={(e) => setUname(e.target.value)}
-            style={{ width: "90%" }}
-            className="text-white bg-gray-800 border border-gray-600 focus:ring-blue-500 focus:border-blue-500"
-          />
+          <label htmlFor="fileInput" className="cursor-pointer">
+            {previewUrl ? (
+              <img
+                src={previewUrl}
+                alt={username}
+                className="rounded-full object-cover"
+                style={{ height: "7rem", width: "7rem" }}
+              />
+            ) : imgUrl ? (
+              <img
+                src={imgUrl}
+                alt={username}
+                className="rounded-full object-cover"
+                style={{ height: "7rem", width: "7rem" }}
+              />
+            ) : (
+              <Avatar size="7rem" name={uuid} variant="beam" />
+            )}
+          </label>
+          {isProfileChange && (
+            <FontAwesomeIcon
+              icon={faPen}
+              className="absolute top-0 right-0 bg-white p-1 rounded-full shadow  -translate-x-2/3 translate-y-2/3"
+              style={{ color: "black" }}
+            />
+          )}
         </div>
-      ) : (
-        <div className="text-white text-2xl">{uname}</div>
-      )}
 
-      <div>
-        <button className="text-white p-2 rounded" onClick={handleCopy}>
-          @{uuid}
-        </button>
-        {copied && (
-          <span className="ml-2 text-green-400">コピーしました！</span>
+        {isProfileChange ? (
+          <div className="flex flex-col">
+            <Input
+              id="username"
+              name="username"
+              type="text"
+              value={uname}
+              onChange={(e) => setUname(e.target.value)}
+              style={{ width: "90%" }}
+              className="text-white bg-gray-800 border border-gray-600 focus:ring-blue-500 focus:border-blue-500"
+            />
+          </div>
+        ) : (
+          <div className="text-white text-2xl">{uname}</div>
         )}
-      </div>
 
-      <br />
+        <div>
+          <button className="text-white p-2 rounded" onClick={handleCopy}>
+            @{uuid}
+          </button>
+          {copied && (
+            <span className="ml-2 text-green-400">コピーしました！</span>
+          )}
+        </div>
+
+        <br />
       </div>
     </>
   );
